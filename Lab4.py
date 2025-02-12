1b1e299d2ca569b785666d673e52bd4460dae85c
aditsangani1 <apsangani3106@gmail.com>
2025-02-12 17:33:52 -0500

    Add files via upload

--- Lab4.py
+++ Lab4.py
@@ -3,11 +3,30 @@ def fibonacci(num):
         return 0
     num1 = 0
     num2 = 1
-    for number in range(1, num + 1):
+    for number in range(1, num):
         num3 = num1 + num2
         num2 = num1
         num1 = num3
     return num3
 
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+def print_prime_factors(num):
+    pass
+
 print(fibonacci(3))
+
+
+
+
+
+
+
+
 
